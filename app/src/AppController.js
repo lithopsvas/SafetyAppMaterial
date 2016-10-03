0394c1f7210bddd7199dc37d36d4e8a0fb5c98c9
Lithopsvas <lithopsvas@mail.ru>
2016-10-03 12:18:20 +0200

    First tries

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -5,40 +5,89 @@
  * @constructor
  */
 function AppController(UsersDataService, $mdSidenav) {
-  var self = this;
+    var self = this;
 
-  self.selected     = null;
-  self.users        = [ ];
-  self.selectUser   = selectUser;
-  self.toggleList   = toggleUsersList;
+    self.selected = null;
+    self.users = [];
+    self.picData= null;
+    self.pictureURI = null;
+    self.selectUser = selectUser;
+    self.toggleList = toggleUsersList;
+    self.changePic = changePic;
 
-  // Load all registered users
+    // Load all registered users
 
-  UsersDataService
+    UsersDataService
         .loadAllUsers()
-        .then( function( users ) {
-          self.users    = [].concat(users);
-          self.selected = users[0];
+        .then(function (users) {
+            self.users = [].concat(users);
+            self.selected = users[0];
         });
 
-  // *********************************
-  // Internal methods
-  // *********************************
+    // *********************************
+    // Internal methods
+    // *********************************
 
-  /**
-   * Hide or Show the 'left' sideNav area
-   */
-  function toggleUsersList() {
-    $mdSidenav('left').toggle();
-  }
+    /**
+     * Hide or Show the 'left' sideNav area
+     */
+    function toggleUsersList() {
+        $mdSidenav('left').toggle();
+    }
 
-  /**
-   * Select the current avatars
-   * @param menuId
-   */
-  function selectUser ( user ) {
-    self.selected = angular.isNumber(user) ? $scope.users[user] : user;
-  }
+    /**
+     * Select the current avatars
+     * @param menuId
+     */
+    function selectUser(user) {
+        self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+    }
+
+    function changePic(event) {
+        console.log("takePicture.onchange");
+        console.log("self.picData");
+        console.log(self.picData);
+        console.log("event");
+        console.log(event);
+        // Get a reference to the taken picture or chosen file
+        var files = event.target.files,
+            file;
+        if (files && files.length > 0) {
+            file = files[0];
+            try {
+                console.log("1");
+                console.log("file");
+                console.log(file);
+                // Create ObjectURL
+                var imgURL = window.URL.createObjectURL(file);
+
+                // Set img src to ObjectURL
+                self.pictureURI = imgURL;
+
+                // Revoke ObjectURL
+               // URL.revokeObjectURL(imgURL);
+            }
+            catch (e) {
+                try {
+                    console.log("2");
+                    // Fallback if createObjectURL is not supported
+                    var fileReader = new FileReader();
+                    fileReader.onload = function (event) {
+                        self.pictureURI = event.target.result;
+                    };
+                    fileReader.readAsDataURL(file);
+                }
+                catch (e) {
+                    console.log("3");
+                    //
+                    var error = document.querySelector("#error");
+                    if (error) {
+                        error.innerHTML = "Neither createObjectURL or FileReader are supported";
+                    }
+                }
+            }
+        }
+    }
 }
 
-export default [ 'UsersDataService', '$mdSidenav', AppController ];
+export default ['UsersDataService', '$mdSidenav', AppController];
--- app/src/app.js
+++ app/src/app.js
@@ -8,20 +8,47 @@ import 'angular-material';
 import AppController from 'src/AppController';
 import Users from 'src/users/Users';
 
-export default angular.module( 'starter-app', [ 'ngMaterial', Users.name ] )
-  .config(($mdIconProvider, $mdThemingProvider) => {
-    // Register the user `avatar` icons
-    $mdIconProvider
-      .defaultIconSet("./assets/svg/avatars.svg", 128)
-      .icon("menu", "./assets/svg/menu.svg", 24)
-      .icon("share", "./assets/svg/share.svg", 24)
-      .icon("google_plus", "./assets/svg/google_plus.svg", 24)
-      .icon("hangouts", "./assets/svg/hangouts.svg", 24)
-      .icon("twitter", "./assets/svg/twitter.svg", 24)
-      .icon("phone", "./assets/svg/phone.svg", 24);
+export default angular.module('SafetyApp', ['ngMaterial', Users.name])
+    .config(($mdIconProvider, $mdThemingProvider) => {
+        // Register the user `avatar` icons
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg", 24)
+            .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+            .icon("twitter", "./assets/svg/twitter.svg", 24)
+            .icon("phone", "./assets/svg/phone.svg", 24);
 
-    $mdThemingProvider.theme('default')
-      .primaryPalette('brown')
-      .accentPalette('red');
-  })
-  .controller('AppController', AppController);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('brown')
+            .accentPalette('red');
+    })
+    .controller('AppController', AppController)
+    .directive("fileread", [function () {
+        return {
+            scope: {
+                fileread: "="
+            },
+            link: function (scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+                        scope.$apply(function () {
+                            scope.fileread = loadEvent.target.result;
+                        });
+                    }
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        }
+    }])
+    .directive('customOnChange', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var onChangeHandler = scope.$eval(attrs.customOnChange);
+                element.bind('change', onChangeHandler);
+            }
+        };
+    });
